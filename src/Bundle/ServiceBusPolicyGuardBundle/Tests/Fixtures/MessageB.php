d019914abfeb0e1ee721781f6529812f881724df
Sebastiaan Stok <s.stok@rollerscapes.net>
2018-02-15 11:06:27 +0100

    Introduce ServiceBusPolicyGuardBundle

---
+++ src/Bundle/ServiceBusPolicyGuardBundle/Tests/Fixtures/MessageB.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Copyright (c) the Contributors as noted in the AUTHORS file.
+ *
+ * This file is part of the Park-Manager project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+namespace ParkManager\Bundle\ServiceBusPolicyGuardBundle\Tests\Fixtures;
+
+final class MessageB
+{
+    private $id;
+
+    public function __construct(int $id)
+    {
+        $this->id = $id;
+    }
+
+    public function id(): int
+    {
+        return $this->id;
+    }
+}
---
+++ src/Bundle/ServiceBusPolicyGuardBundle/Tests/Fixtures/ServiceA.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Copyright (c) the Contributors as noted in the AUTHORS file.
+ *
+ * This file is part of the Park-Manager project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+namespace ParkManager\Bundle\ServiceBusPolicyGuardBundle\Tests\Fixtures;
+
+final class ServiceA
+{
+    public function getId()
+    {
+        return 1;
+    }
+}
