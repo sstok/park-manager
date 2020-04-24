c7fb15a742476b5b2cc8687e0e4bf24555a92b8f
Sebastiaan <s.stok@rollerscapes.net>
2020-04-24 17:30:12 +0200

    Add SuperAdminVoter

---
+++ src/Infrastructure/Security/Voter/SuperAdminVoter.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+namespace ParkManager\Infrastructure\Security\Voter;
+
+use ParkManager\Infrastructure\Security\SecurityUser;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\VoterInterface;
+
+final class SuperAdminVoter implements VoterInterface
+{
+    public function vote(TokenInterface $token, $subject, array $attributes): int
+    {
+        $user = $token->getUser();
+
+        if ($user instanceof SecurityUser && $user->isEnabled() && $user->isSuperAdmin()) {
+            return VoterInterface::ACCESS_GRANTED;
+        }
+
+        return VoterInterface::ACCESS_ABSTAIN;
+    }
+}
---
+++ tests/Infrastructure/Security/Voter/SuperAdminVoterTest.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+namespace ParkManager\Tests\Infrastructure\Security\Voter;
+
+use ParkManager\Infrastructure\Security\SecurityUser;
+use ParkManager\Infrastructure\Security\Voter\SuperAdminVoter;
+use PHPUnit\Framework\TestCase;
+use stdClass;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\VoterInterface;
+use Symfony\Component\Security\Core\User\User;
+
+/**
+ * @internal
+ */
+final class SuperAdminVoterTest extends TestCase
+{
+    /** @test */
+    public function it_grants_access_if_enabled_and_user_admin(): void
+    {
+        $token = $this->createToken(new SecurityUser('e29e2caf-5fc8-4314-9ecd-fd29708b412b', 'Nope', true, ['ROLE_ADMIN', 'ROLE_SUPER_ADMIN']));
+        $voter = new SuperAdminVoter();
+
+        self::assertEquals(VoterInterface::ACCESS_GRANTED, $voter->vote($token, null, []));
+        self::assertEquals(VoterInterface::ACCESS_GRANTED, $voter->vote($token, new stdClass(), []));
+        self::assertEquals(VoterInterface::ACCESS_GRANTED, $voter->vote($token, new stdClass(), ['ACTION_NEW']));
+    }
+
+    private function createToken(?object $user = null): TokenInterface
+    {
+        $tokenProphecy = $this->prophesize(TokenInterface::class);
+        $tokenProphecy->getUser()->willReturn($user);
+
+        return $tokenProphecy->reveal();
+    }
+
+    /**
+     * @test
+     * @dataProvider provideAbstainedAccessFor
+     */
+    public function it_abstains_access_when(object $user): void
+    {
+        $token = $this->createToken($user);
+        $voter = new SuperAdminVoter();
+
+        self::assertEquals(VoterInterface::ACCESS_ABSTAIN, $voter->vote($token, null, []));
+    }
+
+    public function provideAbstainedAccessFor(): iterable
+    {
+        yield 'not a super admin' => [new SecurityUser('e29e2caf-5fc8-4314-9ecd-fd29708b412b', 'Nope', true, ['ROLE_ADMIN'])];
+
+        yield 'not an admin' => [new SecurityUser('e29e2caf-5fc8-4314-9ecd-fd29708b412b', 'Nope', true, ['ROLE_USER'])];
+
+        yield 'access not enabled' => [new SecurityUser('e29e2caf-5fc8-4314-9ecd-fd29708b412b', 'Nope', false, ['ROLE_ADMIN', 'ROLE_SUPER_ADMIN'])];
+
+        yield 'not of the right type' => [new User('hello', 'nope')];
+    }
+}
