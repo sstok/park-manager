2a5e5d6f300a053f44ba2ceb9ec540683d2a424f
Sebastiaan <s.stok@rollerscapes.net>
2021-05-11 16:03:21 +0200

    [UI][Admin] Allowing transferring webhosting space
    to a different owner

---
+++ src/UI/Web/Form/Type/Webhosting/Space/TransferWebhostingSpaceForm.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+namespace ParkManager\UI\Web\Form\Type\Webhosting\Space;
+
+use ParkManager\Application\Command\Webhosting\Space\TransferSpaceToOwner;
+use ParkManager\Domain\Webhosting\Space\Space;
+use ParkManager\UI\Web\Form\Type\MessageFormType;
+use ParkManager\UI\Web\Form\Type\OwnerSelector;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+final class TransferWebhostingSpaceForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->add('owner', OwnerSelector::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefault(
+            'command_factory',
+            static fn (array $fields, Space $model): object => new TransferSpaceToOwner($model->id, $fields['owner']->id),
+        );
+    }
+
+    public function getBlockPrefix(): string
+    {
+        return 'webhosting_space_transfer';
+    }
+
+    public function getParent(): string
+    {
+        return MessageFormType::class;
+    }
+}
