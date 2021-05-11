f49ed0e5016b6212cd6f1a1af4be7793d672fd4d
Sebastiaan <s.stok@rollerscapes.net>
2021-05-11 15:56:25 +0200

    [UI][Admin] Allowing registering webhosting space

---
+++ src/UI/Web/Action/Admin/Webhosting/Space/RegisterWebhostingSpace.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+namespace ParkManager\UI\Web\Action\Admin\Webhosting\Space;
+
+use ParkManager\UI\Web\Form\Type\Webhosting\Space\RegisterWebhostingSpaceForm;
+use ParkManager\UI\Web\Response\RouteRedirectResponse;
+use ParkManager\UI\Web\Response\TwigResponse;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class RegisterWebhostingSpace
+{
+    #[Route(path: 'webhosting/space/register', name: 'park_manager.admin.webhosting.space.register', methods: ['POST', 'GET'])]
+    public function __invoke(Request $request, FormFactoryInterface $formFactory): RouteRedirectResponse | TwigResponse
+    {
+        $form = $formFactory->create(RegisterWebhostingSpaceForm::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            return RouteRedirectResponse::toRoute('park_manager.admin.webhosting.space.list')
+                ->withFlash('success', 'flash.webhosting_space.registered');
+        }
+
+        return new TwigResponse('admin/webhosting/space/register.html.twig', $form);
+    }
+}
---
+++ src/UI/Web/Form/Type/Webhosting/Plan/WebhostingPlanSelector.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+namespace ParkManager\UI\Web\Form\Type\Webhosting\Plan;
+
+use ParkManager\Domain\Webhosting\Constraint\Plan;
+use ParkManager\Domain\Webhosting\Constraint\PlanRepository;
+use ParkManager\UI\Web\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+final class WebhostingPlanSelector extends AbstractType
+{
+    private PlanRepository $planRepository;
+
+    public function __construct(PlanRepository $planRepository)
+    {
+        $this->planRepository = $planRepository;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'label' => 'label.plan',
+            'resultset' => $this->planRepository->all(),
+            'choice_label' => static fn (Plan $plan): string => $plan->getLabel(\Locale::getDefault()),
+            'choice_vary' => [\get_class($this->planRepository), \Locale::getDefault()],
+        ]);
+    }
+
+    public function getBlockPrefix(): string
+    {
+        return 'webhosting_plan_selector';
+    }
+
+    public function getParent(): string
+    {
+        return EntityType::class;
+    }
+}
