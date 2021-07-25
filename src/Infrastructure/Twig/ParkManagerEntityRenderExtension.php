321190af76c5538aa2c64ef7e0df5075d77e3160
Sebastiaan Stok <s.stok@rollerscapes.net>
2021-07-25 11:41:13 +0200

    [Infrastructure] Add Twig EntityRenderExtension
    This Twig extension allows to render entity short, detailed, link
    and it's label

---
+++ src/Infrastructure/Twig/ParkManagerEntityRenderExtension.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+namespace ParkManager\Infrastructure\Twig;
+
+use ParkManager\Infrastructure\Service\EntityRenderer;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+final class ParkManagerEntityRenderExtension extends AbstractExtension
+{
+    public function __construct(private EntityRenderer $entityRenderer)
+    {
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('render_entity_short', [$this->entityRenderer, 'short'], ['is_safe' => 'all']),
+            new TwigFunction('render_entity_detailed', [$this->entityRenderer, 'detailed'], ['is_safe' => 'all']),
+            new TwigFunction('render_entity_link', [$this->entityRenderer, 'link'], ['is_safe' => 'all']),
+            new TwigFunction('render_entity_label', [$this->entityRenderer, 'getEntityLabel'], ['is_safe' => 'html']),
+        ];
+    }
+}
